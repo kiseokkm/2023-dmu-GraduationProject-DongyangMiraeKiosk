1209bd22190a8ac9094c256b8c111efbf502798c
kiseokkm <311kskim@gmail.com>
2023-09-27 16:18:35 +0900

    분실물찾기 기능 신고/찾기 부분 나눔

---
+++ Smart-Campus(Register)_final15/src/main/java/LostThi/LostThings.java
@@ -0,0 +1,57 @@
+package LostThi;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LostThings extends JPanel {
+
+    public LostThings() {
+        setLayout(new BorderLayout());
+
+        JLabel headline = new JLabel("분실물찾기", SwingConstants.CENTER);
+        headline.setFont(new java.awt.Font("Malgun Gothic", java.awt.Font.BOLD, 40));
+        headline.setBorder(BorderFactory.createEmptyBorder(20, 0, 20, 0));
+        add(headline, BorderLayout.NORTH);
+
+        JPanel buttonsPanel = new JPanel(new GridLayout(2, 1, 10, 10));
+
+        JButton btnReport = createButtonWithImage("신고", "/LostThings/FindSearch1.jpeg");
+        btnReport.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                LostThingsReport reportFrame = new LostThingsReport();
+                reportFrame.setVisible(true);
+            }
+        });
+        buttonsPanel.add(btnReport);
+
+        JButton btnFind = createButtonWithImage("찾기", "/LostThings/Report.jpg");
+        btnFind.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                LostThingsFind findFrame = new LostThingsFind();
+                findFrame.setVisible(true);
+            }
+        });
+        buttonsPanel.add(btnFind);
+
+        add(buttonsPanel, BorderLayout.CENTER);
+    }
+
+    private JButton createButtonWithImage(String text, String imagePath) {
+        JButton button = new JButton(text);
+        ImageIcon icon = new ImageIcon(getClass().getResource(imagePath));
+        Image img = icon.getImage().getScaledInstance(100, 40, Image.SCALE_SMOOTH);
+        button.setIcon(new ImageIcon(img));
+        button.setFont(new java.awt.Font("Malgun Gothic", java.awt.Font.BOLD, 18));
+        button.setBackground(Color.LIGHT_GRAY);
+        button.setOpaque(true);
+        button.setBorderPainted(false);
+        button.setPreferredSize(new Dimension(150, 50));
+        button.setHorizontalTextPosition(JButton.CENTER);
+        button.setVerticalTextPosition(JButton.BOTTOM);
+        return button;
+    }
+}
---
+++ Smart-Campus(Register)_final15/src/main/java/LostThi/LostThingsFind.java
@@ -0,0 +1,13 @@
+package LostThi;
+
+import javax.swing.JFrame;
+
+public class LostThingsFind extends JFrame {
+
+    public LostThingsFind() {
+        setTitle("분실물 찾기");  // 창의 제목 설정
+        setSize(400, 300);  // 창의 크기 설정
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);  // 창 닫을 때의 동작 설정
+        setLocationRelativeTo(null);  // 화면 가운데 창을 위치시킴
+    }
+}
