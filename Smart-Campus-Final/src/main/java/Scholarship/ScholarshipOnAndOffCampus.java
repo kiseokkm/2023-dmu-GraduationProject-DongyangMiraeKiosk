1c9f507931f60990588582a99f3ef3dca1e902e2
kiseokkm <311kskim@gmail.com>
2023-10-27 19:40:35 +0900

    교내외장학금 기능 페이지 추가

---
+++ Smart-Campus-Final/src/main/java/Scholarship/ScholarshipOnAndOffCampus.java
@@ -0,0 +1,9 @@
+package Scholarship;
+
+import javax.swing.JPanel;
+
+public class ScholarshipOnAndOffCampus extends JPanel {
+    public ScholarshipOnAndOffCampus() {
+        // 여기에 교내외장학금 관련 내용과 UI 요소를 추가하세요.
+    }
+}
