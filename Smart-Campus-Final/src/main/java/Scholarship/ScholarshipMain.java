cf5e15532db81b6d4a5854a6ad496724b0c97b95
kiseokkm <311kskim@gmail.com>
2023-10-27 19:40:01 +0900

    등록/장학 메인페이지 추가

---
+++ Smart-Campus-Final/src/main/java/Scholarship/ScholarshipMain.java
@@ -0,0 +1,25 @@
+package Scholarship;
+
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+import java.awt.BorderLayout;
+
+public class ScholarshipMain extends JPanel {
+    private TuitionPayment tuitionPayment; 
+    private ScholarshipOnAndOffCampus scholarshipOnAndOffCampus; 
+    private JTabbedPane tabbedPane;
+
+    public ScholarshipMain() {
+        setLayout(new BorderLayout());
+
+        tabbedPane = new JTabbedPane();
+
+        tuitionPayment = new TuitionPayment();
+        scholarshipOnAndOffCampus = new ScholarshipOnAndOffCampus();
+
+        tabbedPane.addTab("등록금납부", tuitionPayment);
+        tabbedPane.addTab("교내외장학금", scholarshipOnAndOffCampus);
+
+        add(tabbedPane, BorderLayout.CENTER);
+    }
+}
