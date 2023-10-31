c8579274fc09bdda8f625153ec0da3e32bb3cd2f
kiseokkm <311kskim@gmail.com>
2023-11-01 02:48:24 +0900

    장학 부분 페이지 구현

--- Smart-Campus-Final/src/main/java/graduation/GraduationMain.java
+++ Smart-Campus-Final/src/main/java/graduation/GraduationMain.java
@@ -7,6 +7,8 @@ public class GraduationMain extends JPanel {
 
     private JTabbedPane graduationTabs;
     private AssociateDegree pnlProfessionalBachelor; // 전문학사
+    private College pnlCollege; // 학사
+    private HoldOnGraduation pnlHoldOnGraduation; // 졸업보류
 
     public GraduationMain() {
         initGraduationTabs();
@@ -19,7 +21,13 @@ public class GraduationMain extends JPanel {
         pnlProfessionalBachelor = new AssociateDegree();
         graduationTabs.addTab("전문학사", pnlProfessionalBachelor);
 
-        // Add more tabs as needed
+        // 학사 tab
+        pnlCollege = new College();
+        graduationTabs.addTab("학사", pnlCollege);
+        
+        // 졸업보류 tab
+        pnlHoldOnGraduation = new HoldOnGraduation();
+        graduationTabs.addTab("졸업보류", pnlHoldOnGraduation);
 
         setLayout(new BorderLayout());
         add(graduationTabs, BorderLayout.CENTER);
