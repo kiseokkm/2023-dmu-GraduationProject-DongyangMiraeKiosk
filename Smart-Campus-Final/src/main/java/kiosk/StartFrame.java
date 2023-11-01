4d07f067d23c91b32e8dce98c5ba6954015691e2
kiseokkm <311kskim@gmail.com>
2023-11-01 19:23:40 +0900

    날씨 기능 추가

--- Smart-Campus-Final/src/main/java/kiosk/StartFrame.java
+++ Smart-Campus-Final/src/main/java/kiosk/StartFrame.java
@@ -1,22 +1,34 @@
 package kiosk;
 
-import javax.swing.*;
-import java.awt.*;
+import java.awt.Dimension;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
+
+import javax.swing.JOptionPane;
 
 public class StartFrame extends javax.swing.JFrame {
 
     private String appName;
     private javax.swing.JLabel lblCurrentDateTime;
     private Timer dateTimeTimer;
+    private javax.swing.JLabel lblWeatherIcon;
+    private javax.swing.JLabel lblWeatherDescription;
+    private javax.swing.JLabel lblCurrentTemp;
+    private javax.swing.JLabel lblHumidity;
+    private javax.swing.JLabel lblWind;
+    private javax.swing.JLabel lblCity;
+    private javax.swing.JLabel lblCloud;
+    private javax.swing.JLabel lblTempMin;
+    private javax.swing.JLabel lblTempMax;
+
 
     public StartFrame() {
         initComponents();
         app.Global.setAppIcon(this);
         startDateTimeUpdater();
+        updateWeatherInfo();
     }
 
     private void initComponents() {
@@ -41,18 +53,20 @@ public class StartFrame extends javax.swing.JFrame {
         lblTitle.setText("Smart Campus");
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 0;
+        gridBagConstraints.gridy = 15;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTH;
         pnlContainer.add(lblTitle, gridBagConstraints);
 
         lblLogo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/app/customer-logo.jpeg"))); 
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 1;
+        gridBagConstraints.gridy = 20;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
         gridBagConstraints.insets = new java.awt.Insets(60, 60, 60, 60);
         pnlContainer.add(lblLogo, gridBagConstraints);
 
+        btnStart.setFont(btnStart.getFont().deriveFont(btnStart.getFont().getSize()+3f)); // 폰트 크기를 10pt 증가시킵니다.
+        btnStart.setPreferredSize(new Dimension(300, 100)); // 버튼의 크기를 400x100으로 조절합니다.
         btnStart.setText("동양미래대학교 방문을 환영합니다.");
         btnStart.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -64,12 +78,51 @@ public class StartFrame extends javax.swing.JFrame {
         gridBagConstraints.gridy = 3;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.SOUTH;
         pnlContainer.add(btnStart, gridBagConstraints);
+        // Weather components initialization
+        lblWeatherIcon = new javax.swing.JLabel();
+        lblWeatherDescription = new javax.swing.JLabel();
+        lblCurrentTemp = new javax.swing.JLabel();
+        lblHumidity = new javax.swing.JLabel();
+        lblWind = new javax.swing.JLabel();
+        lblCity = new javax.swing.JLabel();
+        lblCloud = new javax.swing.JLabel();
+        lblTempMin = new javax.swing.JLabel();
+        lblTempMax = new javax.swing.JLabel();
 
-        // Add the current date and time label here
+        // Position the weather components below the welcome button (assuming 'btnStart' is the welcome button)
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        
+        gridBagConstraints.gridy += 1; // Adjusting the positioning to be below the welcome button
+        pnlContainer.add(lblWeatherIcon, gridBagConstraints);
+        
+        gridBagConstraints.gridy++;
+        pnlContainer.add(lblWeatherDescription, gridBagConstraints);
+        
+        gridBagConstraints.gridy++;
+        pnlContainer.add(lblCurrentTemp, gridBagConstraints);
+        
+        gridBagConstraints.gridy++;
+        pnlContainer.add(lblHumidity, gridBagConstraints);
+        
+        gridBagConstraints.gridy++;
+        pnlContainer.add(lblWind, gridBagConstraints);
+        
+        gridBagConstraints.gridy++;
+        pnlContainer.add(lblCity, gridBagConstraints);
+        
+        gridBagConstraints.gridy++;
+        pnlContainer.add(lblCloud, gridBagConstraints);
+        
+        gridBagConstraints.gridy++;
+        pnlContainer.add(lblTempMin, gridBagConstraints);
+        
+        gridBagConstraints.gridy++;
+        pnlContainer.add(lblTempMax, gridBagConstraints);
         lblCurrentDateTime.setFont(lblCurrentDateTime.getFont().deriveFont(lblCurrentDateTime.getFont().getSize() + 15f));
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 2;
+        gridBagConstraints.gridy = 10;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTH;
         pnlContainer.add(lblCurrentDateTime, gridBagConstraints);
 
@@ -117,4 +170,65 @@ public class StartFrame extends javax.swing.JFrame {
     private javax.swing.JLabel lblLogo;
     private javax.swing.JLabel lblTitle;
     private javax.swing.JPanel pnlContainer;
+
+    private void updateWeatherInfo() {
+        String apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=seoul&appid=20c2fc60cba00523d17f5e65e232b5af";
+        try {
+            java.net.URL url = new java.net.URL(apiUrl);
+            java.net.HttpURLConnection connection = (java.net.HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            
+            java.io.BufferedReader reader = new java.io.BufferedReader(new java.io.InputStreamReader(connection.getInputStream()));
+            StringBuilder response = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+            reader.close();          
+            org.json.JSONObject jsonObject = new org.json.JSONObject(response.toString());
+            org.json.JSONObject main = jsonObject.getJSONObject("main");
+            org.json.JSONObject weather = jsonObject.getJSONArray("weather").getJSONObject(0);
+            org.json.JSONObject wind = jsonObject.getJSONObject("wind");
+            org.json.JSONObject clouds = jsonObject.getJSONObject("clouds");
+            
+            String iconCode = weather.getString("icon").substring(0, 2);
+            
+            // Set the weather icon based on the weather condition
+            String weatherMain = weather.getString("main").toLowerCase();
+            String iconPath;
+            switch (weatherMain) {
+                case "clear":
+                    iconPath = "/icons/sunny.png";
+                    break;
+                case "clouds":
+                    iconPath = "/icons/cloud.png";
+                    break;
+                case "rain":
+                    iconPath = "/icons/rain.png";
+                    break;
+                default:
+                    iconPath = "/icons/weather.png"; // default icon if the condition is not matched
+                    break;
+            }
+            
+            // Resize the image to 300x300
+            java.awt.Image originalImage = new javax.swing.ImageIcon(getClass().getResource(iconPath)).getImage();
+            java.awt.Image resizedImage = originalImage.getScaledInstance(50, 50, java.awt.Image.SCALE_DEFAULT);
+            
+            lblWeatherIcon.setIcon(new javax.swing.ImageIcon(resizedImage));
+
+
+           
+            lblCurrentTemp.setText(Math.round(main.getDouble("temp") - 273.15) + "º");
+            lblHumidity.setText("습도: " + main.getInt("humidity") + "%");
+            lblWind.setText("바람: " + wind.getDouble("speed") + " m/s");
+            lblCity.setText("서울");
+            lblCloud.setText("구름: " + clouds.getInt("all") + "%");
+            lblTempMin.setText("최저 온도: " + Math.round(main.getDouble("temp_min") - 273.15) + "º");
+            lblTempMax.setText("최고 온도: " + Math.round(main.getDouble("temp_max") - 273.15) + "º");
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
